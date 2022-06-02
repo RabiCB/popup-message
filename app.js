f32fef411ab5d534d3ee0b9c56e2cf60de124aa3
Rabindra Bhandari (Chandra) <97612446+RabiCB@users.noreply.github.com>
2022-06-02 23:58:19 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+        let pops=document.getElementById("popup");
+        function openpopup(){
+            pops.classList.add("open-popup");
+        }
+        function closepopup(){
+            pops.classList.remove("open-popup");
+        };
+    
+  
